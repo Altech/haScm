d0e736084a97c21cacd8e40576d96c621c3e6660
Altech <takeno.sh@gmail.com>
2013-05-02 01:56:31 +0900

    1文字をパース。

--- repl.hs
+++ repl.hs
@@ -1,7 +1,17 @@
 module Main where
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: "  ++ show err
+  Right _ -> "Found value."
 
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn $ "Hello, " ++ args !! 0
+  putStrLn (readExpr (args !! 0))
+  return ()
