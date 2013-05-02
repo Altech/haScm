76912451d1a3d4804f3a617932dd682619603681
Altech <takeno.sh@gmail.com>
2013-05-03 03:35:49 +0900

    評価器を部分的に実装（基本型の評価、四則演算の関数適用）。

--- repl.hs
+++ repl.hs
@@ -3,7 +3,6 @@ import System.Environment
 import Text.Parsec hiding (spaces)
 import Control.Monad (liftM)
 import Data.Char (chr)
-import qualified Data.Map as Map
 import Numeric (readOct, readHex)
 
 data LispVal = Atom String
@@ -11,7 +10,24 @@ data LispVal = Atom String
              | DottedList [LispVal] LispVal
              | Number Integer
              | String String
-             | Bool Bool deriving  (Show)
+             | Bool Bool
+
+-- Show
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList hd tl) = "(" ++ unwordsList hd ++ " . " ++ showVal tl ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+-- Parser
 
 symbol :: Parsec String u Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -25,11 +41,11 @@ parseStringEscapedUnicode = liftM (chr . read) $ char '\\' >> count 4 digit
 parseStringEscapedASCII :: Parsec String u Char
 parseStringEscapedASCII = liftM convert $ char '\\' >> noneOf ""
   where
-    controlChars = Map.fromList [('n','\n'),
-                                 ('t','\t'),
-                                 ('r','\r'),
-                                 ('0','\0')]
-    convert c = case Map.lookup c controlChars of
+    controlChars =  [('n','\n'),
+                     ('t','\t'),
+                     ('r','\r'),
+                     ('0','\0')]
+    convert c = case lookup c controlChars of
       Just v -> v
       Nothing -> c
 
@@ -93,13 +109,43 @@ parseExpr = parseNumber <|> parseAtom <|> parseString <|> parseQuoted <|> do
   char ')'
   return x
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
+    
+-- Evaluation
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _)   = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+  if null parsed
+  then 0
+  else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+                     
 
 main :: IO ()
-main = do
-  args <- getArgs
-  putStrLn (readExpr (args !! 0))
-  return ()
+main = getArgs >>= print . eval . readExpr . head
