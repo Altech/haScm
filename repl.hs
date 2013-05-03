a35ce79c00f67f4460e9fff7af8a3b854b7797f6
Altech <takeno.sh@gmail.com>
2013-05-03 19:05:43 +0900

    数値比較関数、文字列比較関数、AND、OR関数を追加。

--- repl.hs
+++ repl.hs
@@ -134,7 +134,35 @@ primitives = [("+", numericBinop (+)),
               ("list?", \v -> return $ case head v of List _ -> Bool True; _ -> Bool False),
               ("symbol?", \v -> return $ case head v of Atom _ -> Bool True; _ -> Bool False),
               ("boolean?", \v -> return $ case head v of Number _ -> Bool True; _ -> Bool False),
-              ("string?", \v -> return $ case head v of Number _ -> Bool True; _ -> Bool False)]
+              ("string?", \v -> return $ case head v of Number _ -> Bool True; _ -> Bool False),
+              ("=", numBoolBinop (==)),
+              ("<", numBoolBinop (<)),
+              (">", numBoolBinop (>)),
+              (">=", numBoolBinop (>=)),
+              ("<=", numBoolBinop (<=)),
+              ("&&", boolBoolBinop (&&)),
+              ("||", boolBoolBinop (||)),
+              ("string=?", strBoolBinop (==)),
+              ("string>?", strBoolBinop (>)),
+              ("string<?", strBoolBinop (<)),
+              ("string<=?", strBoolBinop (<=)),
+              ("string>=?", strBoolBinop (>=))]
+
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2
+                             then throwError $ NumArgs 2 args
+                             else do 
+                               left  <- unpacker $ args !! 0
+                               right <- unpacker $ args !! 1
+                               return $ Bool $ left `op` right
+
+
+numBoolBinop :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
+numBoolBinop  = boolBinop unpackNum
+strBoolBinop :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
+strBoolBinop  = boolBinop unpackStr
+boolBoolBinop :: (Bool -> Bool -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBoolBinop = boolBinop unpackBool
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
@@ -147,6 +175,17 @@ unpackNum (String n) = let parsed = reads n in
   else return $ fst $ parsed !! 0
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s)   = return $ show s
+unpackStr notString  = throwError $ TypeMismatch "string" notString
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
 
 -- Error
 data LispError = NumArgs Integer [LispVal]
