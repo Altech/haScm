0490815ee540e5740c6982a14de0258c3a8f7eba
Altech <takeno.sh@gmail.com>
2013-05-08 20:15:20 +0900

    〈データ〉パーサーを作成。

--- Parser.hs
+++ Parser.hs
@@ -1,8 +1,8 @@
 module Scheme.Parser where
 
-import Control.Monad (liftM)
 import Control.Applicative ((<|>))
-import System.IO
+import Control.Monad (liftM)
+import Data.List (insert)
 import Text.Trifecta hiding (spaces)
 import Text.Trifecta.Delta (Delta(Lines))
 
@@ -13,10 +13,12 @@ data LispVal = Symbol String
              | Number Integer
              | String String
              | Bool Bool
-             | Port Handle
+             -- | Port Handle
              -- | PrimitiveFunc ([LispVal] -> ThrowsError LispVal) 
              -- | IOFunc ([LispVal] -> IOThrowsError LispVal)
              | Func {params :: [String], vararg :: (Maybe String), body :: [LispVal], closure :: Env}
+
+type Env = String -- IORef [(String, IORef LispVal)]
 
 -- type ThrowsError = Either LispError
 -- type IOThrowsError = ErrorT LispError IO
@@ -29,7 +31,7 @@ showVal (Symbol name) = name
 showVal (Number contents) = show contents
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
-showVal (Port _) = "<IO port>"
+-- showVal (Port _) = "<IO port>"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 -- showVal (DottedList _head _tail) = "(" ++ unwordsList _head ++ " . " ++ showVal _tail ++ ")"
 -- showVal (PrimitiveFunc _) = "<primitive>"
@@ -42,14 +44,46 @@ instance Show LispVal where show = showVal
 unwordsList = unwords . map showVal
 
 
--- Environment
-type Env = String -- IORef [(String, IORef LispVal)]
+-- Parsers
 
--- Test
 spaces = do space; skipMany space
 
-parseDefinition = undefined
-parseSyntaxDefinition = undefined
+{- External representations
+ ⟨Datum⟩ is what the read procedure (section 6.6.2) successfully parses. Note that any string that parses as an ⟨expression⟩ will also parse as a ⟨datum⟩.
+
+   ⟨datum⟩ → ⟨simple datum⟩ | ⟨compound datum⟩ 
+   ⟨simple datum⟩ → ⟨boolean⟩ | ⟨number⟩ | ⟨character⟩ | ⟨string⟩ | ⟨symbol⟩
+   ⟨symbol⟩ → ⟨identifier⟩
+   ⟨compound datum⟩ → ⟨list⟩ | ⟨vector⟩
+   ⟨list⟩ −→ (⟨datum⟩*) | (⟨datum⟩+ . ⟨datum⟩) | ⟨abbreviation⟩
+   ⟨abbreviation⟩ → ⟨abbrev prefix⟩ ⟨datum⟩ 
+   ⟨abbrev prefix⟩ → ’ | ` | , | ,@
+   ⟨vector⟩ → #(⟨datum⟩*)
+
+-}
+
+parseDatum :: Parser LispVal -- Especially, it returns subset of LispVal. (e.g. Procedure won't be returned.)
+parseDatum = parseSimpleDatum <|> parseCompoundDatum
+
+parseSimpleDatum :: Parser LispVal
+parseSimpleDatum = parseBoolean <|> parseNumber <|> parseCharacter <|> parseString' <|> parseSymbol
+
+parseCompoundDatum = parseList -- <|> parseVector
+
+--- Symple Datum ... it is called ⟨self-evaluating⟩.
+parseBoolean = (string "#t" <|> string "#f") >>= conv
+  where conv bool = case bool of 
+          "#t" -> return $ Bool True
+          "#f" -> return $ Bool False
+
+parseNumber = liftM Number $ decimal -- [TODO] support Hex, Oct, Bin and sign.
+
+parseCharacter :: Parser LispVal
+parseCharacter = liftM Character $ do -- [Full]
+  string "#\\"
+  (string "space" >> return ' ') <|> (string "newline" >> return '\n') <|> anyChar
+
+parseString' = liftM String $ stringLiteral -- [TODO] Check exact syntax.
 
 parseSymbol = do
   first <- letter <|> specialInitial 
@@ -59,39 +93,29 @@ parseSymbol = do
     specialInitial = oneOf "!$%&*/:<=>?^_~"
     specialSubsequent = oneOf "+-.@"
 
+-- Compound Datum
+parseList = parseNormalList <|> parseDottedList <|> parseAbbreviation
+  where parseNormalList = do 
+          char '(' 
+          ls <- sepBy parseDatum spaces
+          char ')'
+          return $ List ls
+        parseDottedList = do
+          char '(' 
+          ls <- sepBy parseDatum spaces -- [TODO] Check get one at least
+          char '.'
+          las <- parseDatum
+          return $ List $ init ls  ++ [(Cons (last ls) las)]
+        parseAbbreviation = do
+          sym <- parseAbbrevPrefix
+          datum <- parseDatum
+          return $ List [sym, datum]
+        parseAbbrevPrefix = 
+          (string ",@" >> (return $ Symbol "unquote-splicing")) <|>
+          (char '\''   >> (return $ Symbol "quote")) <|>
+          (char '`'    >> (return $ Symbol "quasiquote")) <|>
+          (char ','    >> (return $ Symbol "unquote"))
 
-parseBoolean = do
-  bool <- string "#t" <|> string "#f"
-  case bool of 
-    "#t" -> return $ Bool True
-    "#f" -> return $ Bool False
-
-parseNumber = liftM Number $ decimal -- [TODO] support Hex, Oct, Bin and sign.
-
-parseString' = liftM String $ stringLiteral
-
-parseCharacter = liftM Character $ do -- [Full]
-  string "#\\"
-  (string "space" >> return ' ') <|> (string "newline" >> return '\n') <|> anyChar
-
-parseQuotation = do
-  char '\''
-  x <- parseExpr
-  return $ List [Symbol "quote", x]
-
-parseLiteral = parseBoolean <|> parseNumber <|> parseString' <|> parseCharacter <|> parseQuotation
-
-parseList = do
-  char '('
-  ls <- sepBy parseExpr spaces
-  char ')'
-  return $ List ls
-
-parseExpr :: Parser LispVal
-parseExpr = parseSymbol <|> parseLiteral <|> parseList -- parseQuoted <|> parseSymbol
-
-parseProgram :: Parser [LispVal]
-parseProgram = sepBy parseExpr spaces -- many (parseDefinition <|> parseSyntaxDefinition <|> parseExpr)
 
 -- For debug
 parses :: Show a => Parser a -> String -> String
